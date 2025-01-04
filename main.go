553ef0d42626f061882a89594053ed056a039e50
mklepac90 <mklepac90@gmail.com>
2025-01-04 13:16:44 -0500

    health endpoint

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import (
 	"net/http"
 )
 
+func HealthzHandler (rw http.ResponseWriter,  req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	srv := http.Server{Addr: ":8080", Handler: mux}
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/assets/logo.png", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", HealthzHandler)
+
+	srv := &http.Server{Addr: ":8080", Handler: mux}
 	srv.ListenAndServe();
 }
