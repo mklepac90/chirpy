d351d9d9b4543576b7bf33a11a4b8c69957ef001
mklepac90 <mklepac90@gmail.com>
2025-01-20 14:27:24 -0500

    handle user creation

--- main.go
+++ main.go
@@ -9,39 +9,25 @@ import (
 	"strings";
 	"os";
 	"database/sql";
+	"time";
+
 	"github.com/mklepac90/chirpy/internal/database";
 	"github.com/joho/godotenv";
+	"github.com/google/uuid"
 	_ "github.com/lib/pq";
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	db             *database.Queries
+	platform       string
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) counterHandler (rw http.ResponseWriter,  req *http.Request) {
-	rw.Header().Add("Content-Type", "text/html; charset=utf-8")
-	rw.WriteHeader(http.StatusOK)
-	counter := cfg.fileserverHits.Load()
-	rw.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p><body></html>", counter)))
-}
-
-func (cfg *apiConfig) counterResetHandler (rw http.ResponseWriter,  req *http.Request) {
-	rw.WriteHeader(http.StatusOK)
-	cfg.fileserverHits.Store(0)
-}
-
-func healthzHandler (rw http.ResponseWriter,  req *http.Request) {
-	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	rw.WriteHeader(http.StatusOK)
-	rw.Write([]byte("OK"))
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
@@ -69,6 +55,71 @@ func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
 	respondWithJSON(w, code, errorResponse{
 		Error: msg,
 	})
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) counterHandler (rw http.ResponseWriter,  req *http.Request) {
+	rw.Header().Add("Content-Type", "text/html; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	counter := cfg.fileserverHits.Load()
+	rw.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p><body></html>", counter)))
+}
+
+func (cfg *apiConfig) resetHandler (w http.ResponseWriter,  r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Reset is only allowed in dev environment."))
+		return
+	}
+
+	cfg.fileserverHits.Store(0)
+	cfg.db.Reset(r.Context())
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0 and database reset to initial state."))
+}
+
+func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	type response struct {
+		User
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	user, err := cfg.db.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create user", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, response{
+		User: User{
+			ID:        user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email:     user.Email,
+		},
+	})
+}
+
+func healthzHandler (rw http.ResponseWriter,  req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("OK"))
 }
 
 func getCleanedBody(body string, badWords map[string]struct{}) string {
@@ -123,7 +174,10 @@ func main() {
 	if dbURL == "" {
 		log.Fatal("DB_URL must be set")
 	}
-
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
 	dbConn, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatalf("Error opening database: %s", err)
@@ -133,14 +187,16 @@ func main() {
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		db:             dbQueries,
+		platform:       platform,
 	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("GET /api/healthz", healthzHandler)
 	mux.HandleFunc("POST /api/validate_chirp", chirpHandler)
+	mux.HandleFunc("POST /api/users", apiCfg.createUserHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.counterHandler)
-	mux.HandleFunc("POST /admin/reset", apiCfg.counterResetHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
 
 	srv := &http.Server{Addr: ":8080", Handler: mux}
 	srv.ListenAndServe();
